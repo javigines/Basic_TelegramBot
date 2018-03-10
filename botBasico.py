5089671bbce117e9591d1463ccc9f1186fba308e
javigines <javiginessanchez@hotmail.com>
2018-03-10 20:44:24 +0100

    Update botBasico.py

--- botBasico.py
+++ botBasico.py
@@ -1,60 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Se utiliza la librería de telegram
+# La librería que usamos para comunicarnos con la API que nos proporciona Telegram es:
 # https://github.com/python-telegram-bot/python-telegram-bot
-# Para instalar: pip install python-telegram-bot
-# La API
+# Para empezar, es necesario tener python y pip.
+# Después escribimos en la terminal: pip install python-telegram-bot
+# La API donde podremos ver todos los métodos que nos da la librería es:
 # https://python-telegram-bot.readthedocs.io/en/stable/
 
-# Incluir librerias necesarias (Es como el #define en C)
+# Incluimos solamente las funciones necesarias de las librerías (Es como el #define en C)
 from telegram.ext import Updater, CommandHandler
+# Se podría escribir 'import telegram.ext' e importariamos todo la librería directamente.
 
-# Definición de función en python, los parámetros son necesarios para ciertas funciones mas abajo explico cuales
 
+# Definición de función en python, los parámetros son necesarios para todas las funciones que se ejecuten directamente desde comando de nuestro bot.
 
 def holaMundo(bot, update):
-    # Simplemente se obtiene chat_id de una conversación (Es el identificador de la conversación entre tu y el bot)
-    chat_id = update.message.chat.id
-    # Función de envio de mensajes, estos son los parametros esenciales que hay que introducir
-    bot.sendMessage(chat_id=chat_id, text="Hola Mundo")
+	# Simplemente se obtiene chat_id de una conversación (Es el identificador de la conversación entre tu y el bot).
+	chat_id = update.message.chat.id
+	# Función de envio de mensajes, estos son los parametros esenciales que hay que introducir para enviarlo.
+	bot.sendMessage(chat_id=chat_id, text="Hola Mundo")
 
 
-# Esto es una lectura del token desde un archivo que esta en la misma carpeta del script
+# Esto es una lectura del token desde un archivo que esta en la misma carpeta del script.
 # with open("token.txt", 'r') as tokenFile:
 #	token = tokenFile.read().splitlines()[0]
 
 
-# En lugar de colocar el token en un archivo aparte podemos ponerlo directamente en el código el token pero cuidado con subirlo a github o cosas asi porque es la forma de controlar nuestro bot.
-# Comentar linea siguiente y descomentar las 2 de arriba si se quiere usar así
+# En lugar de colocar el token en un archivo, podemos ponerlo directamente en el código.
+# Cuidado con subirlo a github o repositorios similares porque es la "llave" de acceso a nuestro bot.
+# Comentar linea siguiente y descomentar las 2 de arriba si se quiere usar por archivo.
 token = "1234567890:abcdefghijklmnopqrstuvwxyz"
 
-# Inicializamos vfunciones de nuestra libreria.
-# Aquí estamos introduciendo nuestro token y el número de hilos (que son algo asi como peticiones a la vez) que aceptamos.
+# Inicializamos funciones de nuestra libreria.
+# Aquí estamos introduciendo nuestro token y el número de hilos (que van a ser las peticiones simultáneas que aceptamos).
 updater = Updater(token, workers=200)
-# Estamos guardando el objeto que nos da updater.dispatcher en una variabel más fácil
+# Estamos guardando el objeto que nos da updater.dispatcher en una variable para acceder más facil.
 dispatcher = updater.dispatcher
 
-# Definición del comando al que queremos que el bot responda. Le estamos diciendo que responda al /start y que ejecute la función holaMundo que hemos definido arriba.
-# Lo que os comentaba arriba es que es necesarío incluir los parametros de bot y update en todas las funciones que son llamadas desde aquí, como nuestra función holaMundo
+# Definición del comando al que queremos que el bot responda.
+# Le estamos diciendo que responda al /start y que ejecute la función holaMundo que hemos definido arriba.
 start_handler = CommandHandler('start', holaMundo)
-# Añadimos nuestro objeto comando (lo de arriba) a los comandos que ejecuta el bot.
+# Añadimos nuestro objeto comando, que hemos creado arriba, al listado de comandos que ejecuta el bot.
 dispatcher.add_handler(start_handler)
 
-# En esta línea es realmente donde damos comienzo a nuestro bot.A partir de aquí nuestro bot ya puede emepzar a recibir todos los comandos definidos arriba
+# En esta línea es realmente donde damos comienzo a nuestro bot.
+# A partir de aquí nuestro bot ya puede empezar a recibir todos los comandos definidos arriba.
 updater.start_polling(timeout=1)
-# Esto es un print una cosa muy compleja en python y que tarddaría horas en explicar :D
-print("MainBot Completly Loaded.\nBot Working...")
+# Simplemente hacemos un print para tener una confirmación de que nuestro bot se ha iniciado.
+print("Bot Completly Loaded.\nBot Working...")
 
-# Ejecutamos un bucle infinito para que nuestro bot este corriendo hasta que los servidores dcde coredumped decidan apagarlos, es decir, el bot te aguantará vivo un par de minutos
+# Utilizamos esta función que nos da la librería para que nuestro bot siga ejecutandose hasta que decidamos detenerlo.
 updater.idle()
 
 
-# Otros bots de ejemplo
+# Otros bots de ejemplo:
 
-# Bot de los eventos Coredumped:
+# Bots Mios (@javigines)
+# Bot de los eventos Coredumped (@CoreDumped_EventsBot):
 # https://github.com/javigines/EventsBot-CoreDumped
-# Bot random utilities:
+# Bot random utilities (@RandomUtils_bot):
 # https://github.com/javigines/Random-TelegramBot
 
 # Bots de @javinator_9889
